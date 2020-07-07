6ced9ad625ff00e8d76665371ddc1c9bf465d7b2
sz16900 <sz16900@my.bristol.ac.uk>
2020-07-07 11:50:24 -0500

    refactor

--- search.js
+++ search.js
@@ -1,14 +1,12 @@
 const img = document.querySelector('img');
 const button = document.querySelector('button');
-button.addEventListener('click', () => {
-  fetch(
+button.addEventListener('click', getCats);
+
+async function getCats() {
+  const response = await fetch(
     'https://api.giphy.com/v1/gifs/translate?api_key=SoxusHXbUvTCUnm4QkdbcKCTfPFlXDE1&s=cats',
     { mode: 'cors' }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      img.src = response.data.images.original.url;
-    });
-});
+  );
+  const catData = await response.json();
+  img.src = catData.data.images.original.url;
+}
